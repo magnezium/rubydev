67d08f2afac95c198d5f7f56ea4cd5ada895d0ee
tropin <tropinkavray@gmail.co>
2017-03-31 20:52:29 +0300

    fix

--- movies2.rb
+++ movies2.rb
@@ -1,26 +1,26 @@
 def movies(film_file = 'movies.txt')
-  massiv = []
-  File.open(film_file).each do |line|
-    massiv << line.downcase
-  end
-
-  string = 'max'
-
-  massiv.each do |line|
+  if File.exist?(film_file)
     massiv = []
-    (line.split(/[|]/)).each do |line|
-       massiv << line.to_s
+    File.open(film_file).each do |line|
+      massiv << line.downcase
     end
-    if massiv[1].include?(string)
-      puts massiv[1] + '*' * ((massiv[7].to_f - 8)*10).round(0)
+    string = 'max'
+    massiv.each do |line|
+      massiv = []
+      (line.split(/[|]/)).each do |line|
+         massiv << line.to_s
+      end
+      if massiv[1].include?(string)
+        puts massiv[1] + '*' * ((massiv[7].to_f - 8)*10).round(0)
+      end
     end
+  else
+    puts "Данного файла не существует"
   end
 end
 
 if ARGV.length < 1
   movies
-elsif File.exist?(ARGV[0])
-    movies ARGV[0]
 else
-  puts "Данного файла не существует"
+  movies ARGV[0]
 end
