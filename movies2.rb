acc9df75a9c483eb35b624020836d7f12ad8bc81
tropin <tropinkavray@gmail.co>
2017-04-01 12:51:28 +0300

    fix

--- movies2.rb
+++ movies2.rb
@@ -1,17 +1,19 @@
 film_file = ARGV[0] || 'movies.txt'
 
+
 unless File.exist?(film_file)
   puts "Данного файла не существует"
-else
-  massiv = []
-  File.open(film_file).each do |line|
-    massiv << line
-  end
-  string = 'Max'
-  massiv.each do |line|
-    massiv = line.split('|')
-    if massiv[1].include?(string)
-      puts massiv[1] + '*' * ((massiv[7].to_f - 8)*10).round(0)
-    end
+  exit
+end
+ 
+massiv = []
+File.open(film_file).each do |line|
+  massiv << line
+end
+string = 'Max'
+massiv.each do |line|
+  massiv = line.split('|')
+  if massiv[1].include?(string)
+    puts massiv[1] + '*' * ((massiv[7].to_f - 8)*10).round(0)
   end
 end
